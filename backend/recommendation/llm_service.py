b7fb337544123eea9a8139cefaa1e976a1191b84
Moumita Paul <paulmoumita248@gmail.com>
2026-06-21 18:43:35 +0530

    feat(llm): Implement load_llm function for Mistral AI connection
    Added a function to connect to Mistral AI with error handling for API key.

--- backend/recommendation/llm_service.py
+++ backend/recommendation/llm_service.py
@@ -11,3 +11,24 @@ load_dotenv()
 LLM_MODEL = "mistral-small-2506"
 
 LLM_TEMPARATURE = 0.5
+
+def load_llm():
+    """Connects to Mistral AI and returns a ready-to-use connection object."""
+
+    print("connecting LLM.....")
+
+    api_key = os.getenv("MISTRAL_API_KEY")
+
+    if not api_key:
+        print("ERROR: MISTRAL_API_KEY not found in .env file")
+        exit()
+
+    llm= ChatMistralAI(
+        model = LLM_MODEL,
+        api_key= api_key,
+        temperature= LLM_TEMPARATURE
+    )
+
+    print("connected..")
+
+    return llm
